223af475bf4ae91d618e780f274f845228163d06
Chris Thompson <cthompson@codingdojo.com>
2022-05-05 11:35:16 -0500

    W1D4 lecture - functions return params

---
+++ Day04/script.js
@@ -0,0 +1,34 @@
+function kungfoo(target, hp){//just says i know how to use kungfoo
+    console.log('punching ' + target  )
+    hp = hp - 10
+    console.log('kicking' , target , hp )
+    hp = hp - 20
+    console.log('jumping ' + target + ' ' +hp)
+    console.log('WINNING!!!')
+    return ('i am the chosen one')
+}
+
+
+//kungfoo(); //actually uses kungfoo
+//var quote = 
+kungfoo('Mr. Anderson',100); //actually uses kungfoo
+//console.log(quote)
+
+// //snippet 1
+// function getFirstNameFromForm (){
+//     var firstName = "Juan";
+//     console.log("The firstName variable has been assigned a value.");
+//     return firstName;
+// }
+// //challenge
+// function calculateTipAmount (billTotal, tipPercent){
+// var tipDue = billTotal * tipPercent;
+//     console.log("If you see this, this is just after tipDue was calculated");
+//     return tipDue;
+// }
+//     calculateTipAmount(140, .20);
+
+
+
+    
+    
